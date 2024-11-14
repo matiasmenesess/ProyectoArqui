882db2a91f23cb6552042c472f8bc7f1dd99b015
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-14 16:37:21 -0500

    Update decode.v

--- decode.v
+++ decode.v
@@ -44,20 +44,23 @@ module decode (
 		endcase
 	assign {RegSrc, ImmSrc, ALUSrc, MemtoReg, RegW, MemW, Branch, ALUOp} = controls;
 	always @(*)
-		if (ALUOp) begin
-			case (Funct[4:1])
-				4'b0100: ALUControl = 2'b00;
-				4'b0010: ALUControl = 2'b01;
-				4'b0000: ALUControl = 2'b10;
-				4'b1100: ALUControl = 2'b11;
-				default: ALUControl = 2'bxx;
-			endcase
-			FlagW[1] = Funct[0];
-			FlagW[0] = Funct[0] & ((ALUControl == 2'b00) | (ALUControl == 2'b01));
-		end
-		else begin
-			ALUControl = 2'b00;
-			FlagW = 2'b00;
-		end
+	    if (ALUOp) begin
+	        case (Funct[4:1])
+	            4'b0100: ALUControl = 3'b000;    
+	            4'b0010: ALUControl = 3'b001;    
+	            4'b0000: ALUControl = 3'b010;    
+	            4'b1100: ALUControl = 3'b011;    
+	            4'b1010: ALUControl = 3'b101;    
+	            4'b1000: ALUControl = 3'b110;    
+	            4'b1001: ALUControl = 3'b111;    
+	            default: ALUControl = 3'bxxx;
+	        endcase
+	        FlagW[1] = Funct[0];
+	        FlagW[0] = Funct[0] & ((ALUControl == 3'b000) | (ALUControl == 3'b001));
+	    end
+	    else begin
+	        ALUControl = 3'b000;
+	        FlagW = 2'b00;
+	    end
 	assign PCS = ((Rd == 4'b1111) & RegW) | Branch;
 endmodule
