4b955d69601524f8b441a1b3b31a4db6c6d4f09f
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-14 16:31:29 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,20 +1,20 @@
 module alu (
     input wire [31:0] SrcA,
     input wire [31:0] SrcB,
-    input wire [1:0] ALUControl,
+    input wire [2:0] ALUControl,
     output reg [31:0] ALUResult,
     output reg [3:0] ALUFlags
 );
     always @(*) begin
         case (ALUControl)
-            2'b00: ALUResult = SrcA + SrcB;
-            2'b01: ALUResult = SrcA - SrcB;
-            2'b10: ALUResult = SrcA & SrcB;
-            2'b11: ALUResult = SrcA | SrcB;
+            3'b000: ALUResult = SrcA + SrcB;
+            3'b001: ALUResult = SrcA - SrcB;
+            3'b010: ALUResult = SrcA & SrcB;
+            3'b011: ALUResult = SrcA | SrcB;
+            3'b100: ALUResult = SrcA ^ SrcB;
             default: ALUResult = 32'b0;
         endcase
-
-        // Set flags
+        
         ALUFlags[0] = (ALUResult == 0);
         ALUFlags[1] = ALUResult[31];
         ALUFlags[2] = 0;
