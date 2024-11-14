f6762f4c41ee5b92989731e3764440d288a45cee
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-14 17:17:15 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -5,29 +5,25 @@ module alu (
     output reg [31:0] ALUResult,
     output reg [3:0] ALUFlags
 );
+    wire [32:0] sum;  
+
     always @(*) begin
         case (ALUControl)
             3'b000: ALUResult = SrcA + SrcB;
             3'b001: ALUResult = SrcA - SrcB;
-            3'b010: ALUResult = SrcA & SrcB;
-            3'b011: ALUResult = SrcA | SrcB;
-            3'b100: ALUResult = SrcA ^ SrcB;
-            3'b101: begin
-                ALUResult = SrcA - SrcB;
-                ALUFlags[0] = (ALUResult == 0);
-                ALUFlags[1] = ALUResult[31];
-            end
-            3'b110: begin
-                ALUResult = SrcA & SrcB;
-                ALUFlags[0] = (ALUResult == 0);
-                ALUFlags[1] = ALUResult[31];
-            end
-            3'b111: begin
-                ALUResult = SrcA ^ SrcB;
-                ALUFlags[0] = (ALUResult == 0);
-                ALUFlags[1] = ALUResult[31];
-            end
+            3'b010: ALUResult = SrcB;
+            3'b011: ALUResult = SrcA & SrcB;
+            3'b100: ALUResult = SrcA | SrcB;
+            3'b101: ALUResult = SrcA ^ SrcB;
             default: ALUResult = 32'b0;
         endcase
     end
+
+    assign sum = SrcA + (ALUControl[1] ? ~SrcB + 1 : SrcB); 
+
+    assign neg = ALUResult[31];
+    assign zero = (ALUResult == 32'b0);
+    assign carry = (ALUControl[1] == 1'b0) & sum[32];
+    assign overflow = (ALUControl[1] == 1'b0) & ~(SrcA[31] ^ SrcB[31] ^ ALUControl[0]) & (SrcA[31] ^ sum[31]);
+    assign ALUFlags = {neg, zero, carry, overflow};
 endmodule
