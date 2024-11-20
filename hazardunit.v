8e590d9d1d84f69636986f6d2ad71e94c4afa8dc
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-20 17:45:57 -0500

    Update hazardunit.v

--- hazardunit.v
+++ hazardunit.v
@@ -1,26 +1,24 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/20/2024 05:09:39 PM
-// Design Name: 
-// Module Name: hazardunit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module hazardunit(
+	RegWriteW,
+	RegWriteM,
+	Match,
+	ForwardBE,
+	ForwardAE
+);
+	input wire RegWriteW;
+	input wire RegWriteM;
+	input wire [3:0] Match;
+	output wire [1:0] ForwardAE;
+	output wire [1:0] ForwardBE;
 
-    );
+	wire Match_1E_M, Match_1E_W, Match_2E_M, Match_2E_W;
+
+	assign {Match_1E_M, Match_1E_W, Match_2E_M, Match_2E_W} = Match;
+
+	ForwardAE = Match_1E_M & RegWriteM ? 2'b10 : (Match_1E_W & RegWriteW ? 2'b01 : 2'b00) 
+	ForwardBE = Match_2E_M & RegWriteM ? 2'b10 : (Match_2E_W & RegWriteW ? 2'b01 : 2'b00)
+
+    //FALTAN MAS COSAS AVANZAR MIS BROS
+
 endmodule
+
