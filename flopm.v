1af41c11892ea00d128986c2401cf56939e814cd
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-20 17:46:30 -0500

    Update flopm.v}

--- flopm.v
+++ flopm.v
@@ -1,1 +1,37 @@
 
+module flopm(
+	clk,
+	reset,
+	PCSrcM,
+	RegWriteM,
+	MemtoRegM,
+	PCSrcW,
+	RegWriteW,
+	MemtoRegW,
+);
+	input wire clk;
+	input wire reset;
+	input wire PCSrcM;
+	input wire RegWriteM;
+	input wire MemtoRegM;
+	output reg PCSrcW;
+	output reg RegWriteW;
+	output reg MemtoRegW;
+
+	always @ (posedge clk or posedge reset)
+	begin
+		if (reset)
+		begin
+			PCSrcW <= 0;
+			RegWriteW <= 0;
+			MemtoRegW <= 0;
+		end
+		else
+		begin
+			PCSrcW <= PCSrcM;
+			RegWriteW <= RegWriteM;
+			MemtoRegW <= MemtoRegM;
+		end
+	end
+	
+endmodule
