05277f458e0d83336e4c98dcf494cc483bab3c6d
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-24 20:00:34 -0500

    error de coma

--- dmem.v
+++ dmem.v
@@ -3,14 +3,14 @@ module dmem (
 	we,
 	a,
 	wd,
-	rd,
+	rd
 );
 	input wire clk;
 	input wire we;
 	input wire [31:0] a;
 	input wire [31:0] wd;
 	output wire [31:0] rd;
-	reg [31:0] RAM [63:0];
+	reg [31:0] RAM [255:0];
 	assign rd = RAM[a[31:2]];
 	always @(posedge clk)
 		if (we)
