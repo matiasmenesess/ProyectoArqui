21eea2acd396cac238965264b647395c886436ba
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-25 11:24:34 -0500

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -8,5 +8,35 @@ module datapath (
 	WriteData,
 	ReadData
 );
-
+	input wire clk;
+	input wire reset;
+	output wire [31:0] PC;
+	input wire [31:0] InstrF;
+	output wire MemWrite;
+	output wire [31:0] ALUResult;
+	output wire [31:0] WriteData;
+	input wire [31:0] ReadData;
+	wire [3:0] ALUFlags; // cuando agreguemos Q debemos cambiarlo a 4:0, tdv
+	
+	controller c(
+		.clk(clk),
+		.reset(reset),
+		.ALUFlags(ALUFlags),
+		.InstrD(InstrF),
+		.BranchTakenE(BranchTakenE),
+		.RegSrcD(RegSrc),
+		.RegWriteW(RegWriteW),
+		.RegWriteM(RegWriteM),
+		.ImmSrcD(ImmSrc),
+		.ALUSrcE(ALUSrc),
+		.ALUControlE(ALUControl),
+		.MemWriteM(MemWrite),
+		.MemtoRegW(MemtoReg),
+		.PCSrcW(PCSrc),
+		.PCSrcE(PCSrcE),
+       		.PCSrcD(PCSrcD),
+        	.PCSrcM(PCSrcM),
+		.MemtoRegE(MemtoRegE)
+	);
+	
 endmodule
