fe00647dd21c3d6656299ee83a6889ac235968e1
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-24 20:01:36 -0500

    cambie el archivo memfile

--- imem.v
+++ imem.v
@@ -5,6 +5,6 @@ module imem (
 	input wire [31:0] a;
 	output wire [31:0] rd;
 	reg [31:0] RAM [63:0];
-	initial $readmemh("memfile.asm", RAM);
+	initial $readmemh("memfile.mem", RAM);
 	assign rd = RAM[a[31:2]];
 endmodule
