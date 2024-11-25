abe976c615ac75722832be510463703876486c1a
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-25 11:41:55 -0500

    para acabar arm lo que falta

--- datapath.v
+++ datapath.v
@@ -1,6 +1,36 @@
-module datapath (
-	
-);
+    module datapath (
+        clk,
+        reset,
+        RegSrcD,
+        RegWriteW,
+        ImmSrcD,
+        ALUSrcE,
+        ALUControlE,
+        MemtoRegW,
+        PCSrcW,
+        ALUFlags,
+        PCF,
+        InstrF,
+        ALUOutM,
+        WriteDataE,
+        ReadDataM, 
+        ExtImmD, 
+        RD1D, 
+        RD2D, 
+        BranchTakenE,
+        MemtoRegE,
+        Match_1E_M, //hazard
+        Match_1E_W, 
+        Match_2E_M,
+        Match_2E_W, 
+        Match_12D_E,
+        ForwardAE,
+        ForwardBE,
+        StallF,
+        StallD,
+        FlushE
+    );
+    
 
 	
 endmodule
